c593a38b1257582adcb4f80f5b2049dc5d523dd8
Eduard Ghenea <edymare97@yahoo.com>
2024-12-12 20:27:10 +0200

    add java spring boot proj with 4 dependencies

---
+++ eat-it/src/test/java/ide/eatit/EatItApplicationTests.java
@@ -0,0 +1,13 @@
+package ide.eatit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
