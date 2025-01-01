9d7ca2775a4c5c9ab99279284f8b78909491160c
Eduard Ghenea <edymare97@yahoo.com>
2025-01-01 15:21:52 +0200

    it is finally on it's feet

--- eat-it/src/main/java/ide/eatit/EatItApplication.java
+++ eat-it/src/main/java/ide/eatit/EatItApplication.java
@@ -3,7 +3,7 @@ package ide.eatit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ide.eatit")
 public class EatItApplication {
 
     public static void main(String[] args) {
